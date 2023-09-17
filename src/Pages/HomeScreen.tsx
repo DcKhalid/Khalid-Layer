e113e11eb8ce623869712bb1f9f222e6af22c791
Adekunle Khalid <adekunlekhlaid@gmail.com>
2023-09-18 00:22:37 +0100

    first commit

---
+++ src/Pages/HomeScreen.tsx
@@ -0,0 +1,42 @@
+import React from "react"
+import PricingComponent from "../Components/PricingComponent";
+import Achievements from "./Achievements";
+import Additional from "./Additional";
+import Demovid from "./DemoVid";
+import Features from "./Features";
+import HOW from "./HOW";
+import Hero from "./Hero";
+import News from "./News";
+import Pricing from "./Pricing";
+import Layer from "./Pricing";
+import ScreenShots from "./ScreenShots";
+import Team from "./Team";
+import Template from "./Template";
+import Welcome from "./Welcome";
+import Twitter from "./Twitter";
+import Subscribe from "./Subscribe";
+import InTouch from "./InTouch";
+
+const HomeScreen = () => {
+  return (
+    <div>
+      <Hero />
+      <Welcome />
+      <Features />
+      <HOW />
+      <Additional />
+      <Pricing />
+      <ScreenShots />
+      <Achievements />
+      <Demovid />
+      <Template />
+      <Team />
+      <News />
+      <Twitter />
+      <Subscribe />
+      <InTouch />
+    </div>
+  );
+};
+
+export default HomeScreen;
